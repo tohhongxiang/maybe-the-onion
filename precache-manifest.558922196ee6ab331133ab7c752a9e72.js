bf2ec55583afbaa7abc8e22a0c093c51b2865343
tohhongxiang123 <tohhongxiang@gmail.com>
2020-06-02 14:43:13 +0800

    Updates

---
+++ precache-manifest.558922196ee6ab331133ab7c752a9e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cab6beb9fb8d19246c99bec3312d21",
+    "url": "/maybe-the-onion/index.html"
+  },
+  {
+    "revision": "d81847c8ce08cdf4890a",
+    "url": "/maybe-the-onion/static/css/main.cda36a3e.chunk.css"
+  },
+  {
+    "revision": "e7fc15323db71b7ea691",
+    "url": "/maybe-the-onion/static/js/2.414c1403.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maybe-the-onion/static/js/2.414c1403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81847c8ce08cdf4890a",
+    "url": "/maybe-the-onion/static/js/main.4c365dcb.chunk.js"
+  },
+  {
+    "revision": "b857e11c4cec7c3f87d7",
+    "url": "/maybe-the-onion/static/js/runtime-main.5f29a409.js"
+  }
+]);
